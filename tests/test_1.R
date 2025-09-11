b14bd841aa45b1e6597b42c3811917be8ef68ef8
Mohammed Farhan N P <65911037+mohammedfarhannp@users.noreply.github.com>
2025-09-11 19:57:22 +0530

    Create test_1.R
    Day 1 of learning R

---
+++ tests/test_1.R
@@ -0,0 +1,53 @@
+# var_1 <- 20
+# var_2 <- 44
+# print(var_1+var_2)
+
+# var_0 <- c(01,2,3,4,5)
+# print(var_0)
+
+# var_3 <- readline("Enter a Number: ") # Str Input
+# print(var_3)
+
+# var_4 <- seq(0,100) # Create a sequence of number from 0 to 100
+# print(var_4)
+
+# var_5 <- readline("Enter a Number: ")
+# var_6 <- readline("Enter a Number: ")
+# var_7 <- as.integer(var_5) + as.integer(var_6)
+# print(var_7)
+
+# var_8 <- as.integer(readline("Enter a number: ")) #Integer Input
+# print(var_8)
+
+# var_9 = seq(100,1,-1)
+# print(var_9)
+
+# for(i in 1: 4) {
+#   print(i*i)
+# }
+
+# var_10 <- seq(7,500,7) # Numeric Vector
+# print(var_10)
+# print(length(var_10)) # Length of Numeric Vector
+# print(var_10[1:25]) # Similar to String Slicing in Python
+# print(var_10[c(7,71)]) # use of C function to pass multiple indexes
+# print(sum(var_10[35:length(var_10)-1])) # Sum
+# print(mean(var_10[35:length(var_10)-1])) # Average
+# var_11 <- mean(var_10[35:length(var_10)-1])
+# print(length(var_10[var_10>var_11])) # Number of elements in vector whose value is greater than average value
+# print(sum(var_10[var_10>var_11])) # sum of elements in vector whose value is greater than average value
+
+
+# var_12 <- c("A", "B", "C", "D", "E", "F")
+# print(var_12)
+# print(var_12[c(3,6)])
+
+#for(i in var_12){
+#  print(i)
+#}
+
+# var_13 <- c(160.4, 157.8, 150, 140, 173.2, 156.7)
+# print(var_13)
+
+# var_14 <- data.frame(Name=var_12, Height=var_13)
+# print(var_14)
