b043d5da52926816e0d63f48b2215ee63da434ad
Mohammed Farhan N P <65911037+mohammedfarhannp@users.noreply.github.com>
2025-09-16 22:09:43 +0530

    Create Identify_DataType.R

---
+++ lessons/Identify_DataType.R
@@ -0,0 +1,8 @@
+x <- 8 # integer data type
+z <- "Hello" # string data type
+a = c(1, 2, 3, 4, 5) # vector data type
+
+# use class() function to identify the type of data of a variable.
+print(class(x))
+print(class(z))
+print(class(a))
