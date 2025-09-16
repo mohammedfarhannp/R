c061738b88662aed4f5c1d76c92a22d5fe668acb
Mohammed Farhan N P <65911037+mohammedfarhannp@users.noreply.github.com>
2025-09-16 22:02:53 +0530

    Create test-2.R
    Day 2 of Learning R

---
+++ tests/test-2.R
@@ -0,0 +1,56 @@
+# https://github.com/mohammedfarhannp/R
+
+# print(class(0.872)) # class() determines type of data | similar to type() in python
+
+# print(round(sqrt(2), 2))
+
+# x <- 10
+# y <- 8
+# print(x > y)
+# print(x == y)
+# print(x >= y)
+
+# And 
+# print(TRUE & TRUE)
+# print(FALSE & TRUE)
+
+# Or 
+# print(TRUE | FALSE)
+# print(TRUE | TRUE)
+# print(FALSE | FALSE)
+
+# Negation
+# print(!TRUE)
+
+# List
+# var_1 <- list(Name="John", Age=25, Marks=c(90, 40, 70))
+# print(var_1)
+# print(var_1$Name)
+# print(var_1[["Age"]])
+# print(var_1[[3]])
+
+# Data Frame
+# students <- data.frame(Name = c("A", "B", "C"), Age=c(25, 30, 30),Marks=c(85, 90, 88))
+# print(students$Name) # Prints Column Name
+# print(students[1,]) # Prints First Row
+# print(students[2,]) # Prints Second Row
+# print(students[,3]) # Prints Third Column
+
+# Matrix
+# var_2 = matrix(c(1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31), nrow=4, ncol=4)
+# print(var_2)
+
+# var_2[1,2]
+# var_2[,3]
+# var_2[2,]
+
+# Array
+# arr <- array(1: 12, dim=c(2,3,2))
+# arr[1, , ]
+# arr[, 1, ]
+# arr
+
+# Question Answered!
+# var_3 = matrix(seq(20,31), nrow=3, ncol=4)
+# var_3[2, ]
+
