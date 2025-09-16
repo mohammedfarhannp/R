4bafc1eb58cca68962ffd7d78e0f2bf5d6545b7c
Mohammed Farhan N P <65911037+mohammedfarhannp@users.noreply.github.com>
2025-09-16 22:07:03 +0530

    Create Logical_Operations.R

---
+++ lessons/Logical_Operations.R
@@ -0,0 +1,13 @@
+x <- 8
+10 -> y
+
+print(x > y) # greater than
+print(x < y) # less than
+print(x == y) # equal to
+
+print(x >= y) # Greater than or equal to
+print(x <= y) # Less than or equal to
+
+print(True & True) # and
+print(True | False) # or
+print(!True) # Negation
