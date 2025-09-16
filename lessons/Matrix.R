df483fe00ad2cb62cefac699abfab47efdac315b
Mohammed Farhan N P <65911037+mohammedfarhannp@users.noreply.github.com>
2025-09-16 23:07:16 +0530

    Create Matrix.R

---
+++ lessons/Matrix.R
@@ -0,0 +1,9 @@
+# Matrix like in Maths
+var_1 = matrix(1: 16, nrow=4, ncol=4)
+
+print(var_1) # Prints Entire Matrix
+
+print(var_1[1, 2]) # Prints first row, second column element.
+print(var_1[ ,3]) # Prints third Column
+print(var_1[2, ]) # Prints Second Row
+
